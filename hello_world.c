97786a7299a3aefa8b45a540fe0be2aacf0c2273
OrionRandD <43448489+OrionRandD@users.noreply.github.com>
2023-07-25 09:52:04 -0300

    hello_world

---
+++ hello_world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// add standard input-output library
+int main()
+// type is "int" and identifier is "main"
+{
+    // printf is a function sitting in "stdio.h"
+    // "Hello World" is a string
+    // "\n" is a control new-line character
+    printf("Hello World\n");
+    // "return 0" is executed if everything goes ok
+    return 0;
+}
